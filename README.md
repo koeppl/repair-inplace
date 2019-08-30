edb356682d0d12783bf0101d87d2bcfa145481c9
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2019-08-30 20:47:16 +0900

    clean up code

--- README.md
+++ README.md
@@ -12,17 +12,16 @@ You can change the compile flags for a release or debug build in the `Makefile`.
 Running the program `repair-inplace` without parameters displays its parameters and its usage.
 Add `-DVERBOSE` and `-DVVERBOSE` to enhance verbose or very verbose messages, outputting, among others, the created rules.
 
-The program `repair-inplace` takes as input a file, which it treats as a byte stream.
+The program `repair-inplace` takes as input a file, and treats this file as a byte stream.
 So the input alphabet size is the byte alphabet.
 We simplified the implementation in that we store the input in a 16-bit array of fixed size.
-Hence, the program works whenever the sum of all different terminals and non-terminal is less than 2^16.
-It is easy to enhance the program with a dynamic resizing of the bit width of the text by using a variable bit vector like 
-the `IntVector` of [bit span](https://github.com/tudocomp/bit_span).
+Hence, the program works whenever the sum of all different terminals and non-terminal is at most 2^16.
+It is easy to enhance the program with a dynamic resizing of the bit width of the text by using an integer vector supporting dynamic bit widths like the `IntVector` of [bit span](https://github.com/tudocomp/bit_span).
 We refrained from adding this feature as the program is already very slow, taking already one hour for computing Re-Pair on 1 MiB text files.
-This slow running time looks inevitable, as it is an O(n^2) time algorithm, where the time bound looks tight.
-We did not implement the word-packed variant in the paper, as the number of involved operations look to make the algorithm actually slower.
+This slow running time looks inevitable, as the algorithm described in the paper takes O(n^2) time, at it looks likely that this time bound is tight.
+We did not implement the word-packed variant described in the paper, as the number of involved operations seem to make the algorithm actually slower.
 
-As this is a proof-of-concept implementation, it lacks of crucial features like
+As this is a proof-of-concept implementation, it lacks crucial features:
 
 * there is no output - however you can extract the rules and the start symbol from the log messages
 * there is no decompression verifying that the text is restorable from the output
--- repair.cpp
+++ repair.cpp
@@ -10,10 +10,12 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+// CAVEAT: the character and bigram types are fixed. We should represent a character as an integer of variable bit width
 using bigram_t = uint32_t;
 using char_type = uint16_t; // representing the alphabet size
 
-bigram_t make_bigram(uint32_t a, uint32_t b) {
+// CAVEAT: make_bigram works only as long as `a` and `b` do not exceed 2^16.
+bigram_t make_bigram(uint32_t a, uint32_t b) { 
 	DCHECK_LT(a, 1ULL<<16);
 	DCHECK_LT(b, 1ULL<<16);
 	return (a<<16) + b;
@@ -103,6 +105,7 @@ struct FrequencyTable {
 
 };
 
+// only for debug purpose 
 std::string text_to_string(const char_type*const text, size_t text_length) {
 	std::stringstream ss;
 	ss << "Text: ";
@@ -113,6 +116,7 @@ std::string text_to_string(const char_type*const text, size_t text_length) {
 	return ss.str();
 }
 
+// only for debug purpose 
 size_t character_frequency(const char_type*const text, size_t text_length, char_type a) {
 	size_t frequency = 0;
 	for(size_t i = 0; i < text_length; ++i) {
@@ -122,6 +126,7 @@ size_t character_frequency(const char_type*const text, size_t text_length, char_
 }
 
 
+// only for debug purpose 
 size_t bigram_frequency(const char_type*const text, size_t text_length, char_type a, char_type b) {
 	bool matched_first = false;
 	size_t frequency = 0;
@@ -146,8 +151,8 @@ size_t bigram_frequency(const char_type*const text, size_t text_length, char_typ
 
 int main(int argc, char *argv[]) {
 	
-	size_t additional_memory = 200;
-	size_t text_length = 0;
+	size_t additional_memory = 200; // default number of bytes available
+	size_t text_length = 0; // size of the text we deal with, subject to shrinkage after a turn
 	std::string filename;
 
 
@@ -206,7 +211,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	try {
-	    const size_t total_text_length = fs::file_size(filename);  // size of the text we deal with, subject to shrinkage
+	    const size_t total_text_length = fs::file_size(filename);  // size of the entire file
 	    if(text_length == 0) { text_length = total_text_length; }
 	    else { text_length = std::min(text_length, total_text_length); }
 	} catch(fs::filesystem_error& e) {
@@ -220,13 +225,13 @@ int main(int argc, char *argv[]) {
 	std::cout << "file name: " << filename << std::endl;
 	std::cout << "byte size of an entry: " << sizeof(Entry) << std::endl;
 	try {
-		// size_t text_length = fs::file_size(filename);  // size of the text we deal with, subject to shrinkage
 		const size_t memory_budget = text_length+additional_memory; // constant size of total memory we have
 		char_type maximum_character = 0; // the currently maximum value a character can have
 
 		DCHECK_LT(text_length, memory_budget); // need at least some memory
 
-		char_type*const text = new char_type[memory_budget];
+		// CAVEAT: the text has a fixed bit width
+		char_type*const text = new char_type[memory_budget]; // create our working space and store the text in it
 		std::ifstream is(filename);
 		for(size_t i = 0; i < text_length && is; ++i) {
 			text[i] = is.get();
@@ -244,8 +249,8 @@ int main(int argc, char *argv[]) {
 			DCHECK_GT(available_entries, 2); // need at least some memory
 
 			++round_k;
-			std::cout << "Round " << round_k << std::endl;
-			std::cout << "frequency Storage: " << available_entries << std::endl;
+			std::cout << "Round " << round_k << std::endl; // round and f_k as in the paper
+			std::cout << "f_k: " << available_entries << std::endl; 
 
 			Entry*const entries = reinterpret_cast<Entry*>(text+text_length);
 			for(size_t i = 0; i < available_entries; ++i) { entries[i].clear(); }
@@ -261,7 +266,7 @@ int main(int argc, char *argv[]) {
 					pos = helperTable.insert(bigram);
 					DCHECK_NE(pos, (-1ULL));
 				} 
-				// else {
+				// else { // CAVEAT: use this code to start already counting when populating helperTable
 				// 	helperTable[pos].increment();
 				// }
 				// std::cout << pos << std::endl;
@@ -287,6 +292,7 @@ int main(int argc, char *argv[]) {
 						}
 					} 
 					VVLOG << "remainder helperTable before sort:" << helperTable.to_string();
+					// CAVEAT: use a better sorting algorithm
 					std::sort(entries, entries+available_entries, [](const Entry&a, const Entry&b) {
 							// true if a > b
 							return a.frequency() > b.frequency();
@@ -385,6 +391,7 @@ int main(int argc, char *argv[]) {
 				char_type*const D = text+text_length;
 				size_t D_length = 0; // length of table D
 
+				// CAVEAT: we could merge the preceding characters and the succeeding characters step into a single code with a few lambda functions
 				// take preceding characters
 				for(size_t i = 0; i < text_length; ++i) {
 					if(i > 0 && text[i] == maximum_character) {
